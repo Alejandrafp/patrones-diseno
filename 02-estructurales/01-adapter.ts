904c9baf9cdaa9514179e5ea07892279215f36c3
MAFP <44504798+Alejandrafp@users.noreply.github.com>
2025-05-15 11:12:04 -0600

    Implementation of Adapter

--- 02-estructurales/01-adapter.ts
+++ 02-estructurales/01-adapter.ts
@@ -10,3 +10,13 @@
  *
  * https://refactoring.guru/es/design-patterns/adapter
  */
+
+// import { LocalLogger } from "./adapter-files/local-logger.ts";
+import { DenoLoggerAdapter } from './adapter-files/logger-adapter.ts';
+
+
+const logger = new DenoLoggerAdapter('01-adapter.ts');
+
+logger.writeLog('Todo esta funcionando correctamente.');
+logger.writeWarning('Precaucion, parece que algo no esta trabajando como esperabamos.');
+logger.writeError('Tenemos un gran problema. Por favor resolverlo lo mas pronto posible.');
--- 02-estructurales/adapter-files/logger-adapter.ts
+++ 02-estructurales/adapter-files/logger-adapter.ts
@@ -1,3 +1,36 @@
-import { Logger } from 'jsr:@deno-library/logger';
+import Logger from "https://deno.land/x/logger@v1.1.7/logger.ts";
 
 // TODO: Implementar el LoggerAdapter
+
+
+interface ILoggerAdapter {
+    file: string;
+
+    writeLog: (msg: string) => void;
+    writeWarning: (msg: string) => void;
+    writeError: (msg: string) => void;
+
+}
+
+
+export class DenoLoggerAdapter implements ILoggerAdapter{
+    public file: string;
+    private logger = new Logger();
+
+    constructor(file: string){
+        this.file = file;
+    }
+
+    writeLog(msg: string){
+        this.logger.info(`[${this.file} Log ] ${ msg }`);
+    };
+
+    writeWarning(msg: string){
+        this.logger.warn(`[${this.file} Warning ] ${ msg }`);
+    };
+    writeError(msg: string){
+        this.logger.error(`[${this.file} Error ] ${ msg }`);
+    };
+}
+
+// const logger = new Logger();
