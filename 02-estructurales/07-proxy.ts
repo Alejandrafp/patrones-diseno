5edbbbb01246de0dbb48033706e98d795bff4b74
MAFP <44504798+Alejandrafp@users.noreply.github.com>
2025-05-18 15:10:57 -0600

    Implementation of Proxy

--- 02-estructurales/07-proxy.ts
+++ 02-estructurales/07-proxy.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Patrón Proxy
  * Este patrón se utiliza para controlar el acceso a un objeto, es decir,
@@ -10,3 +11,58 @@
  * https://refactoring.guru/es/design-patterns/proxy
  *
  */
+
+class Player{
+    name: string;
+    level: number;
+
+    constructor(name: string, level: number){
+        this.name = name;
+        this.level = level;
+    }
+}
+
+interface Room {
+    enter(player: Player): void;
+
+}
+
+
+class SecretRoom implements Room{
+enter(player: Player): void{
+    console.log(`%cBienvenido a la sala secreta, ${player.name}`, COLORS.orange);
+    console.log(`%cUn gran enemigo te espera`, COLORS.blue);
+}
+}
+
+//Clase Proxy - Magic Portal
+
+class MagicPortal implements Room{
+    private secretRoom: Room;
+
+    constructor(room: Room){
+        this.secretRoom = room;
+    }
+
+    enter(player: Player): void {
+        if(player.level >= 10){
+            this.secretRoom.enter(player);
+            return;
+        }
+        console.log(`%cLo siento mucho ${player.name}, tu nivel ${player.level}, es muy bajo, necesitas ser nivel 10`, COLORS.red);
+    }
+}
+
+function main(){
+    const portal = new MagicPortal( new SecretRoom()) //Proxy
+    const player1 = new Player('Aventurero A', 5);
+    const player2 = new Player('Aventurero B', 10);
+    
+    console.log(`Aventurero A intenta entrar al portal`, COLORS.blue);
+    portal.enter(player1);
+    portal.enter(player2);
+
+
+}
+
+main();
