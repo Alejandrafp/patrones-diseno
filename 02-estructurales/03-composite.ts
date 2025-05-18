3b013675d2411c9903062c5677e1c0e9c6bd4993
MAFP <44504798+Alejandrafp@users.noreply.github.com>
2025-05-17 22:05:09 -0600

    Implementation of Composite

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,75 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+    showDetails(indent?: string): void;
+
+}
+
+
+class FileU implements FileSystemComponent{
+    private name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+
+    showDetails(indent?: string): void{
+        console.log(`${indent}- Archivo: ${this.name}`);
+    }
+}
+
+class Folder implements FileSystemComponent{
+    private name: string;
+    private contents: FileSystemComponent[] = [];
+
+    constructor(name: string){
+        this.name = name;
+    }
+
+    add(component: FileSystemComponent){
+        this.contents.push(component);
+    }
+
+    showDetails(indent: string = ''): void{
+        console.log(`${indent}+ Carpeta: ${this.name}`);
+        this.contents.forEach(component => component.showDetails(indent + ' '));
+        
+    }
+}
+
+function main(){
+    const file1 = new FileU('archivo1.txt');
+    const file2 = new FileU('archivo2.txt');
+    const file3 = new FileU('archivo3.txt');
+    const file4 = new FileU('archivo4.txt');
+
+    const folder1 = new Folder('Carpeta 1');
+    const folder2 = new Folder('Carpeta 2');
+    const folder3 = new Folder('Carpeta 3');
+    const folder4 = new Folder('Carpeta 4');
+    const folder5 = new Folder('Carpeta 5');
+
+
+
+    folder1.add( file1 );
+    folder1.add( file2 );
+
+    folder2.add( file3 );
+    folder2.add( folder3 );
+    folder2.add( folder5 );
+
+
+    folder3.add( file4 );
+
+    const rootFolder = new Folder('Carpeta ROOT');
+    rootFolder.add(folder1)
+    rootFolder.add(folder2)
+
+    rootFolder.showDetails();
+    
+
+}
+
+main();
