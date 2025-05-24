017d9ec6bc5c5f2d61a22d04697b64c56ef94a35
MAFP <44504798+Alejandrafp@users.noreply.github.com>
2025-05-23 21:36:13 -0600

    Implementation of Observer

--- 03-comportamiento/06-observer.ts
+++ 03-comportamiento/06-observer.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Patrón Observer
  * El patrón Observer es un patrón de diseño de comportamiento que establece
@@ -13,3 +14,70 @@
  *
  * https://refactoring.guru/es/design-patterns/observer
  */
+
+interface Observer{
+    notify(videoTitle: string):void;
+}
+
+class YoutubeChannel{
+    private subscribers: Observer[] =[];
+    private name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+
+    subscribe(observer: Observer): void{
+        this.subscribers.push(observer);
+        console.log(`%cNuevo suscriptor del canal: ${this.name}`, COLORS.orange);
+    }
+    unsubscribe(observer: Observer): void{
+        this.subscribers = this.subscribers.filter(sub => sub === observer);
+        console.log(`%cUn suscriptor se ha dado de baja: ${this.name}`, COLORS.green);
+    }
+
+    uploadVideo(videoTitle: string): void{
+        console.log(`Canal: ${this.name} ha subido un nuevo video %c${videoTitle}`, COLORS.blue);
+
+        for (const subscriber of this.subscribers) {
+            subscriber.notify(videoTitle);
+        }
+    }
+}
+
+class Subscriber implements Observer{
+    private name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+
+    notify(videoTitle: string): void {
+        console.log(`${this.name} ha sido notificado %cNuevo video: %c${videoTitle}`, COLORS.blue, COLORS.purple);
+
+    }
+
+}
+
+function main(){
+    const channel = new YoutubeChannel('Concinando con Ale');
+    const melisa = new Subscriber('Melisa');
+    const carlos = new Subscriber('Carlos');
+    const eduardo = new Subscriber('Eduardo');
+
+    channel.subscribe(melisa);
+    channel.subscribe(carlos);
+
+    channel.uploadVideo('Receta de tamales');
+
+    channel.subscribe(eduardo);
+    channel.uploadVideo('Receta de tacos al pastor');
+
+    channel.unsubscribe(carlos);
+
+    channel.uploadVideo('Receta de pizza');
+
+    console.log('\n\n');
+}
+
+main();
